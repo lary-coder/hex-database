720a1f0d290f7c0306d076c22531066a3080eba0
Laryssa Ribeiro <laryssarib@gmail.com>
2022-07-21 15:10:32 -0300

    9-insert_school.py

---
+++ 0x04-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Python function that inserts a new document
+in a collection based on kwargs
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Returns the new _id"""
+    return mongo_collection.insert_one(kwargs).inserted_id
